dd68a25f765089d287c9d363ff55e2c6cca8cbff
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-06-10 22:21:24 +0600

    projects links added

--- src/Components/ProjectsDetails.js
+++ src/Components/ProjectsDetails.js
@@ -7,6 +7,15 @@ import prj21 from "../Components/images/prj21.png";
 import prj22 from "../Components/images/prj22.png";
 import prj23 from "../Components/images/prj23.png";
 import prj24 from "../Components/images/prj24.png";
+import prj31 from "../Components/images/prj31.png";
+import prj32 from "../Components/images/prj32.png";
+import prj33 from "../Components/images/prj33.png";
+import prj34 from "../Components/images/prj34.png";
+import prj41 from "../Components/images/prj41.png";
+import prj42 from "../Components/images/prj42.png";
+import prj43 from "../Components/images/prj43.png";
+import prj44 from "../Components/images/prj44.png";
+import { Link } from "react-router-dom";
 
 const ProjectsDetails = () => {
   return (
@@ -15,6 +24,23 @@ const ProjectsDetails = () => {
       <h1 className="text-4xl font-bold  ">Projects</h1>
       <div className="tools-ware">
         <h1 className="text-4xl font-bold bg-slate-500 p-4 my-6">Tools Ware</h1>
+        <div className="link-detail">
+          <a href="https://tools-ware.web.app/" className="btn  m-2">
+            Live
+          </a>
+          <a
+            href="https://github.com/MDSHOHAG617/Tools_Ware_Client-side"
+            className="btn  m-2"
+          >
+            Client
+          </a>
+          <a
+            href="https://github.com/MDSHOHAG617/Tools_Ware_Server-side"
+            className="btn  m-2"
+          >
+            Sever
+          </a>
+        </div>
         <div className="grid lg:grid-cols-2  gap-5 m-5">
           <div class="card w-96 glass mx-auto">
             <figure>
@@ -68,6 +94,26 @@ const ProjectsDetails = () => {
         <h1 className="text-4xl font-bold bg-slate-500 p-4 my-6">
           Perfume Gallery
         </h1>
+        <div className="link-detail">
+          <a
+            href="https://assignment-11-node-mongo.web.app/"
+            className="btn  m-2"
+          >
+            Live
+          </a>
+          <a
+            href="https://github.com/MDSHOHAG617/Perfume_Gallery-Client-side"
+            className="btn  m-2"
+          >
+            Client
+          </a>
+          <a
+            href="https://github.com/MDSHOHAG617/Perfume_Gallery-Server-side"
+            className="btn  m-2"
+          >
+            Server
+          </a>
+        </div>
         <div className="grid lg:grid-cols-2  gap-5 m-5">
           <div class="card w-96 glass mx-auto">
             <figure>
@@ -117,14 +163,28 @@ const ProjectsDetails = () => {
           </div>
         </div>
       </div>
-      <div className="perfume-gallery">
+      <div className="brain-train">
         <h1 className="text-4xl font-bold bg-slate-500 p-4 my-6">
           The Brain Train
         </h1>
+        <div className="link-detail">
+          <a
+            href="https://assignment-10-firebase-auth.web.app/"
+            className="btn  m-2"
+          >
+            Live
+          </a>
+          <a
+            href="https://github.com/MDSHOHAG617/The-Brain-Train"
+            className="btn  m-2"
+          >
+            Client
+          </a>
+        </div>
         <div className="grid lg:grid-cols-2  gap-5 m-5">
           <div class="card w-96 glass mx-auto">
             <figure>
-              <img src={prj21} alt="car!" />
+              <img src={prj31} alt="car!" />
             </figure>
           </div>
           <div class="card w-96 glass mx-auto my-auto">
@@ -155,17 +215,76 @@ const ProjectsDetails = () => {
           </div>
           <div class="card w-96 glass mx-auto">
             <figure>
-              <img src={prj22} alt="car!" />
+              <img src={prj32} alt="car!" />
             </figure>
           </div>
           <div class="card w-96 glass mx-auto">
             <figure>
-              <img src={prj23} alt="car!" />
+              <img src={prj33} alt="car!" />
             </figure>
           </div>
           <div class="card w-96 glass mx-auto">
             <figure>
-              <img src={prj24} alt="car!" />
+              <img src={prj34} alt="car!" />
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className="agency">
+        <h1 className="text-4xl font-bold bg-slate-500 p-4 my-6">Agency</h1>
+        <div className="link-detail">
+          <a href="https://mdshohag617.github.io/Agency/" className="btn  m-2">
+            Live
+          </a>
+          <a href="https://github.com/MDSHOHAG617/Agency" className="btn  m-2">
+            Client
+          </a>
+        </div>
+        <div className="grid lg:grid-cols-2  gap-5 m-5">
+          <div class="card w-96 glass mx-auto">
+            <figure>
+              <img src={prj41} alt="car!" />
+            </figure>
+          </div>
+          <div class="card w-96 glass mx-auto my-auto">
+            <h1 className="card-title mx-auto my-5 text-white text-2xl">
+              About Tools Ware
+            </h1>
+            <p className="text-left ml-5 py-5">
+              This is a ful-stack website, in this app I work both frontend and
+              backend. this is an tool store type website where you can
+              parches/order tools/products. for order you should must login An
+              admin can make some difference like add products delete products,
+              make others an admin etc.
+            </p>
+          </div>
+          <div class="card w-96 glass mx-auto my-auto py-8">
+            <h1 className="card-title mx-auto my-5 text-white text-2xl">
+              Features and functionality
+            </h1>
+            <p className="text-left ml-5  ">
+              <li>used Auth state to maintain the login and sign </li>
+              <li>up Used JWT token for </li>
+              <li>verify user Payment Method by using stripe You </li>
+              <li>can order any tools </li>
+              <li>
+                from this site An Admin can manage An admin will make an admin{" "}
+              </li>
+            </p>
+          </div>
+          <div class="card w-96 glass mx-auto">
+            <figure>
+              <img src={prj42} alt="car!" />
+            </figure>
+          </div>
+          <div class="card w-96 glass mx-auto">
+            <figure>
+              <img src={prj43} alt="car!" />
+            </figure>
+          </div>
+          <div class="card w-96 glass mx-auto">
+            <figure>
+              <img src={prj44} alt="car!" />
             </figure>
           </div>
         </div>
